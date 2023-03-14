8a492f678bf6e0ca9c261020e4135a9704727afb
Marcos Capistrano <mcapistrano50@gmail.com>
2023-03-14 15:17:50 -0300

    oi

---
+++ part3/index.js
@@ -0,0 +1,80 @@
+import express, { response } from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello World!</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (note) res.json(note);
+  else res.status(404).end();
+});
+
+const generateID = () => {
+    const maxID = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+
+    return maxID + 1;
+}
+
+app.post("/api/notes", (req, res) => {
+    const body = req.body;
+
+    if(!body.content) {
+        return res.status(400).json({
+            error: 'content missing'
+        });
+    };
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateID()
+    };
+
+    notes = notes.concat(note)
+    
+    res.json(note);
+})
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
